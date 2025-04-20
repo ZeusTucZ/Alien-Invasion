0aa47ae00e885fe0a952e11588a2fd8e61d14df6
ZeusTucZ <lorenzo.orrante@icloud.com>
2025-04-19 23:52:28 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.ship_limit = 3
+        self.ship_limit = 2
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
