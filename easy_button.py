e7dbcfcdedc7d0668ead7b453ba4f4b625dad31e
ZeusTucZ <lorenzo.orrante@icloud.com>
2025-04-27 17:42:58 -0600

    Niveles de dificultad
    Botones para elegir dificultad (Todavía no hacen nada)

---
+++ easy_button.py
@@ -0,0 +1,41 @@
+import pygame.font
+
+class EasyButton:
+    """A class to build buttons for the game."""
+
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = 200, 50
+        self.default_button_color = (0, 55, 0)
+        self.selected_button_color = (0, 135, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center it.
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        self.rect.centery += 100
+        self.rect.centerx -= 250
+
+        # Check if the button is selected
+        self.is_selected = False
+
+        # The button message needs to be prepped only one.
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.default_button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        """Draw blank button and then draw message."""
+        color = self.selected_button_color if self.is_selected else self.default_button_color
+        self.screen.fill(color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
